19ca1fe3ecddef8200c92329b549f008f865d75d
gouriinanda <gourinandakg@gmail.com>
2026-06-10 01:32:13 +0530

    bag store

---
+++ bagstore/src/App.jsx
@@ -0,0 +1,20 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './component/Home'
+
+function App() {
+  
+
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={< Home/>} />
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ bagstore/src/component/Home.jsx
@@ -0,0 +1,91 @@
+//import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <h1 className="m-3 text-warning fs-3"><b>Bag Store</b></h1>
+            <div className="row g-3">
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                <div class="card">
+                  <img src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRsMM8PUjn3i9wAXelLT7h25jaTsOsRtXVoTA9rJZyh67YiEuDhfjdEsmYF_LQKGFG2aGsWQEt8" class="card-img-top" alt="..." style={{ height: "500px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">Tommy Hilfiger Joshua Laptop Backpack</h5>
+                    <p class="card-text">A premium, chic navy blue choice crafted from high-quality polyester
+                        <br/>Price: ₹1,999<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTCWEsrEzoSBDgUs08PinHK2UGarclQqKAEPF7oTJ9UxP_F3DxuGDDRnEvgLyTJkAOWZ263LQSc" class="card-img-top" alt="..." style={{ height: "500px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">Skybags Lumous Laptop Backpack</h5>
+                    <p class="card-text">A stylish and functional laptop backpack designed for everyday use.
+                        <br/>Price: ₹999<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://m.media-amazon.com/images/I/61Rb+rv0lNL._AC_UY1100_.jpg" class="card-img-top" alt="..." style={{ height: "500px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">ADSON Kawaii Girls Travel School Bag</h5>
+                    <p class="card-text">A cute and practical school bag, perfect for daily use.
+                        <br/>Price: ₹1,000<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://m.media-amazon.com/images/I/61Rb+rv0lNL._AC_UY1100_.jpg" class="card-img-top" alt="..." style={{ height: "500px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">ADSON Kawaii Girls Travel School Bag</h5>
+                    <p class="card-text">A cute and practical school bag, perfect for daily use.
+                        <br/>Price: ₹1,000<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://m.media-amazon.com/images/I/61Rb+rv0lNL._AC_UY1100_.jpg" class="card-img-top" alt="..." style={{ height: "500px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">ADSON Kawaii Girls Travel School Bag</h5>
+                    <p class="card-text">A cute and practical school bag, perfect for daily use.
+                        <br/>Price: ₹1,000<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://m.media-amazon.com/images/I/61Rb+rv0lNL._AC_UY1100_.jpg" class="card-img-top" alt="..." style={{ height: "500px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">ADSON Kawaii Girls Travel School Bag</h5>
+                    <p class="card-text">A cute and practical school bag, perfect for daily use.
+                        <br/>Price: ₹1,000<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
