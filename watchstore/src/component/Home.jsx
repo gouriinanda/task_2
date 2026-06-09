725e8945f9cc4a2b5347d3d2752eaab08c069936
gouriinanda <gourinandakg@gmail.com>
2026-06-10 01:03:23 +0530

    watch store

---
+++ watchstore/src/component/Home.jsx
@@ -0,0 +1,72 @@
+//import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <h1 className="m-3 text-warning fs-3"><b>Watch Store</b></h1>
+            <div className="row g-3">
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                <div class="card h-100">
+                  <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTXpivTNwyqXvcQevTSkoPd2zfjnu2k0MFbccE6-RDqxJiF5FyKCf8AlkreYIfaw1XDvtt4FqyM" class="card-img-top" alt="..." style={{ height: "200px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">G-Shock Carbon Core GA2100</h5>
+                    <p class="card-text">Features an ultra-thin, heavy-duty octagonal body reinforced with durable carbon fiber layers.</p>
+                    <a href="#" class="btn btn-primary m-3">Add to Cart</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card h-100">
+                  <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT_zbCwL2bKIYEGJq5nGKP9mp6VHnnA4BFkjA7wu4jdFKfXA6Gi0T4wWfJwU8-KbF3_oSDB-qcK" class="card-img-top" alt="..." style={{ height: "200px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">G-Shock Digital Watch</h5>
+                    <p class="card-text">A sporty, sophisticated timepiece showing off a striking gold and silver stainless steel design layout.</p>
+                    <a href="#" class="btn btn-primary m-3">Add to Cart</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card h-100">
+                  <img src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRhzcuMlDQ5l7fPNe2MtdgMgcLU3cUf0j9n-TK7aIJxsgfb3Vxjookam5rFoPl8YI-k8zKf4co" class="card-img-top" alt="..." style={{ height: "200px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">Neo IV Tan Leather</h5>
+                    <p class="card-text">A minimalist, budget-friendly option matching a silver-white dial with an elegant tan leather band strap.</p>
+                    <a href="#" class="btn btn-primary m-3">Add to Cart</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card h-100">
+                  <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT_zbCwL2bKIYEGJq5nGKP9mp6VHnnA4BFkjA7wu4jdFKfXA6Gi0T4wWfJwU8-KbF3_oSDB-qcK" class="card-img-top" alt="..." style={{ height: "200px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">G-Shock Digital Watch</h5>
+                    <p class="card-text">A sporty, sophisticated timepiece showing off a striking gold and silver stainless steel design layout.</p>
+                    <a href="#" class="btn btn-primary m-3">Add to Cart</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card h-100">
+                  <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTXpivTNwyqXvcQevTSkoPd2zfjnu2k0MFbccE6-RDqxJiF5FyKCf8AlkreYIfaw1XDvtt4FqyM" class="card-img-top" alt="..." style={{ height: "200px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">G-Shock Carbon Core GA2100</h5>
+                    <p class="card-text">Features an ultra-thin, heavy-duty octagonal body reinforced with durable carbon fiber layers.</p>
+                    <a href="#" class="btn btn-primary m-3">Add to Cart</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
