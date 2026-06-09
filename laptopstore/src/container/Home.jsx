51b5a63dd6940ac0cb9719012016c4c318891034
gouriinanda <gourinandakg@gmail.com>
2026-06-10 01:50:53 +0530

    laptop store

---
+++ laptopstore/src/container/Home.jsx
@@ -0,0 +1,91 @@
+//import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <h1 className="m-3 text-warning fs-3"><b>Laptop Store</b></h1>
+            <div className="row g-3">
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                <div class="card">
+                  <img src="https://m.media-amazon.com/images/I/510uTHyDqGL._AC_UF1000,1000_QL80_.jpg" class="card-img-top" alt="..." style={{ height: "300px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">NEW Microsoft Surface Laptop (7th Edition)</h5>
+                    <p class="card-text">A cutting-edge Copilot+ PC powered by the 12-core Qualcomm Snapdragon X Elite processor.
+                        <br/>Price: ₹1,63,990<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZQ5ZIIveHfwjsHQ1q68xfgUZDg-WK-0VHHw&s" class="card-img-top" alt="..." style={{ height: "300px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">HP 15-fd0588TU Intel Core i5</h5>
+                    <p class="card-text">A highly efficient everyday worker packing a 13th Gen Intel Core i5 processor and a massive 1TB SSD
+                        <br/>Price: ₹49,990<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRiRfw7NM5W7fLBiuiboGG-U26mDWp4zhNRM6BPBCKFvW9LuBCPjE6lkx9sRSrJOsuDmvx-pcc" class="card-img-top" alt="..." style={{ height: "300px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">HP 15s 13th Gen Intel Core i5</h5>
+                    <p class="card-text">Outfitted with an integrated fingerprint reader and a physical privacy camera shutter for added security.
+                        <br/>Price: ₹54,490<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZQ5ZIIveHfwjsHQ1q68xfgUZDg-WK-0VHHw&s" class="card-img-top" alt="..." style={{ height: "300px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">HP 15-fd0588TU Intel Core i5</h5>
+                    <p class="card-text">A highly efficient everyday worker packing a 13th Gen Intel Core i5 processor and a massive 1TB SSD
+                        <br/>Price: ₹49,990<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRiRfw7NM5W7fLBiuiboGG-U26mDWp4zhNRM6BPBCKFvW9LuBCPjE6lkx9sRSrJOsuDmvx-pcc" class="card-img-top" alt="..." style={{ height: "300px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">HP 15s 13th Gen Intel Core i5</h5>
+                    <p class="card-text">Outfitted with an integrated fingerprint reader and a physical privacy camera shutter for added security.
+                        <br/>Price: ₹54,490<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZQ5ZIIveHfwjsHQ1q68xfgUZDg-WK-0VHHw&s" class="card-img-top" alt="..." style={{ height: "300px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">HP 15-fd0588TU Intel Core i5</h5>
+                    <p class="card-text">A highly efficient everyday worker packing a 13th Gen Intel Core i5 processor and a massive 1TB SSD
+                        <br/>Price: ₹49,990<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
