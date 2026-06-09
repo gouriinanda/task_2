8d3d52e52bbdd22aa0f490d1bd629de2734b85e3
gouriinanda <gourinandakg@gmail.com>
2026-06-10 00:11:00 +0530

    mobile store

---
+++ mobilestore/src/component/Home.jsx
@@ -0,0 +1,81 @@
+//import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <div className="container mt-5">
+                <div className="row">
+                    <h1 className="text-danger fs-3"><b>Mobile Store</b></h1>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card h-100">
+                                    <img src="https://darlingretail.com/cdn/shop/files/iPhone_15_Pink_Pure_Back_iPhone_15_Pink_Pure_Front_2up_Screen__WWEN_cfd96ace-df87-4ab3-a96a-e8e9b13bb7b9_800x.jpg?v=1695104022" class="card-img-top" alt="..." script={{height:"200", objectFit: "cover"}} />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Galaxy S26</h5>
+                                        <p class="card-text">A premium flagship featuring advanced Galaxy AI tools and a stunning 120Hz display.</p>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card h-100">
+                                    <img src="https://www.sciencemuseum.org.uk/sites/default/files/styles/her/public/2018-11/nokia-3210-hero.jpg?h=dd54426e&itok=g19-AGa9" class="card-img-top" alt="..."script={{height: "200", objectFit:"cover"}} />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Nokia 3210 4G</h5>
+                                        <p class="card-text">Nokia is a legendary mobile phone brand known for its durability and simplicity.</p>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card h-100">
+                                    <img src="https://opsg-img-cdn-gl.heytapimg.com/epb/202302/15/SrtL7TwcCqYXBAln.png" class="card-img-top" alt="..."script={{height: "200", objectFit: "cover"}} />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Oppo Reno15 5G</h5>
+                                        <p class="card-text">A budget-friendly smartphone with a powerful processor and a vibrant display.</p>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card h-100">
+                                    <img src="https://image01-in.oneplus.net/media/202504/29/f7dd3061e62632c0f49d1f9c28ccc214.png?x-amz-process=image/format,webp/quality,Q_80" class="card-img-top" alt="..."script={{height: "200", objectFit: "cover"}} />
+                                    <div class="card-body">
+                                        <h5 class="card-title">OnePlus 13 5G</h5>
+                                        <p class="card-text">An elite flagship powered by the high-performance Snapdragon 8 Elite processor and a smooth 2K display..</p>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card h-100">
+                                     <div class="card h-100">
+                                    <img src="https://image01-in.oneplus.net/media/202504/29/f7dd3061e62632c0f49d1f9c28ccc214.png?x-amz-process=image/format,webp/quality,Q_80" class="card-img-top" alt="..."script={{height: "200", objectFit: "cover"}} />
+                                    <div class="card-body">
+                                        <h5 class="card-title">OnePlus 13 5G</h5>
+                                        <p class="card-text">An elite flagship powered by the high-performance Snapdragon 8 Elite processor and a smooth 2K display..</p>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                 <div class="card h-100">
+                                    <img src="https://image01-in.oneplus.net/media/202504/29/f7dd3061e62632c0f49d1f9c28ccc214.png?x-amz-process=image/format,webp/quality,Q_80" class="card-img-top" alt="..."script={{height: "200", objectFit: "cover"}} />
+                                    <div class="card-body">
+                                        <h5 class="card-title">OnePlus 13 5G</h5>
+                                        <p class="card-text">An elite flagship powered by the high-performance Snapdragon 8 Elite processor and a smooth 2K display..</p>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
