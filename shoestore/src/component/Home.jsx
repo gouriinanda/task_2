0f0cd3311ab8f8f7620fdf51249c057e27ce637b
gouriinanda <gourinandakg@gmail.com>
2026-06-10 12:20:38 +0530

    shoe store

---
+++ shoestore/src/component/Home.jsx
@@ -0,0 +1,91 @@
+//import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <h1 className="m-3 text-warning fs-3"><b>ShoeStore</b></h1>
+            <div className="row g-3">
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                <div class="card">
+                  <img src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcS0OX8BSZmvjHD8Q2zycHhF2lYJsxTJaHNbSOVYCRImJlJeKmYxSlmwCEvJhxxwlwxBCEirdbt-" class="card-img-top" alt="..." style={{ height: "200px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">Adidas Duramo 10 Running Shoes</h5>
+                    <p class="card-text">A highly versatile athletic-casual choice sporting a cooling mesh upper made from recycled materials.
+                        <br />Price: ₹ 3,999.00<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRB7VDNSVq5m4CsrnJ7xAUtrk7hCNFDUVxwWVYAiYhJmbA4udNIH0Od-rtfuElNetGZqEOrB6Rd" class="card-img-top" alt="..." style={{ height: "200px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">Kalenji Run One Men's Running Shoes</h5>
+                    <p class="card-text">An ultra-lightweight, budget-friendly shoe designed for casual walking, jogging, or light fitness routines
+                        <br/>Price: ₹ 999<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTo_v92JG1KLILDp310xvyf_YvHb6cMo5SgeBexcD7CVfRuVwTxtW50N2RMlpmCd8A8paqvkD4" class="card-img-top" alt="..." style={{ height: "200px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">Decathlon Running Shoes Jogflow 190.1</h5>
+                    <p class="card-text">A versatile running shoe with excellent cushioning and support for daily training.
+                        <br/>Price: ₹ 1,299<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcS0OX8BSZmvjHD8Q2zycHhF2lYJsxTJaHNbSOVYCRImJlJeKmYxSlmwCEvJhxxwlwxBCEirdbt-" class="card-img-top" alt="..." style={{ height: "200px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">Adidas Duramo 10 Running Shoes</h5>
+                    <p class="card-text">A highly versatile athletic-casual choice sporting a cooling mesh upper made from recycled materials.
+                        <br />Price: ₹ 3,999.00<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRB7VDNSVq5m4CsrnJ7xAUtrk7hCNFDUVxwWVYAiYhJmbA4udNIH0Od-rtfuElNetGZqEOrB6Rd" class="card-img-top" alt="..." style={{ height: "200px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">Kalenji Run One Men's Running Shoes</h5>
+                    <p class="card-text">An ultra-lightweight, budget-friendly shoe designed for casual walking, jogging, or light fitness routines
+                        <br/>Price: ₹ 999<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                 <div class="card">
+                  <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcS0OX8BSZmvjHD8Q2zycHhF2lYJsxTJaHNbSOVYCRImJlJeKmYxSlmwCEvJhxxwlwxBCEirdbt-" class="card-img-top" alt="..." style={{ height: "200px", objectFit: "cover" }} />
+                  <div class="card-body">
+                    <h5 class="card-title">Kalenji Run One Men's Running Shoes</h5>
+                    <p class="card-text">An ultra-lightweight, budget-friendly shoe designed for casual walking, jogging, or light fitness routines
+                        <br/>Price: ₹ 999<br/>
+                    </p>
+                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
